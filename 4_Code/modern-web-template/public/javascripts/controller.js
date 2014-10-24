9190c09dacfb8cf19b51a9e62a2e4f28ad8f372d
Jörg Amelunxen <jamelunx@mail.upb.de>
2014-10-24 13:45:20 +0200

    groups can be shown

--- 4_Code/modern-web-template/public/javascripts/controller.js
+++ 4_Code/modern-web-template/public/javascripts/controller.js
@@ -1,15 +1,34 @@
 controllersModule.controller('GroupCtrl', ['$scope','$http', function($scope,$http) {
+    var that = this;
 
-    this.groups = [{
-        name:"123",name:"345"
-    }];
+    this.groups = [
+        {name:"123"},
+        {name:"345"}
+    ];
 
     this.currentGroup = {name: this.groupName,
                          member: this.groupMember,
                          createdBy: "dummy",
                          notifications: ["the group #name# has been created by an supervisor"]};
 
+    this.getGroups = function(){
+        $http.get('/admin/groups').
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                that.groups = data;
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+
+                that.groups = "Error: Connection error";
+            });
+    };
+
     this.createGroup = function(){
+        that.getGroups();
+
         $http.post('/admin/group', this.currentGroup).
             success(function(data, status, headers, config) {
                 // this callback will be called asynchronously
@@ -18,22 +37,6 @@ controllersModule.controller('GroupCtrl', ['$scope','$http', function($scope,$ht
             error(function(data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
-            });
-    };
-
-    this.getGroups = function(){
-        $http.get('/admin/groups').
-            success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-
-                this.groups = data;
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-
-                this.groups = "Error: Connection error";
             });
     };
 }]);
