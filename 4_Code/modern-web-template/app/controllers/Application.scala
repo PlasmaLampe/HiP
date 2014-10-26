5c829f06f733af331bf468fae77ea3ae401d91c7
Jörg Amelunxen <jamelunx@mail.upb.de>
2014-10-26 13:00:21 +0100

    minor changes

--- 4_Code/modern-web-template/app/controllers/Application.scala
+++ 4_Code/modern-web-template/app/controllers/Application.scala
@@ -4,11 +4,23 @@ import javax.inject.{Inject, Singleton}
 
 import org.slf4j.{Logger, LoggerFactory}
 import play.api.mvc._
+import securesocial.core._
 
 @Singleton
-class Application extends Controller{
+class Application extends Controller  with SecureSocial{
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[Application])
+
+   /*
+  def index = SecuredAction { implicit  request =>
+
+    println(" USER ---- ::: ")
+    println(request.user)
+    println(" USER ---- ::: ")
+
+    Ok(views.html.index())
+  }*/
+
 
   def index = Action {
     logger.info("Serving index page...")
--- 4_Code/modern-web-template/app/services/MyUserService.scala
+++ 4_Code/modern-web-template/app/services/MyUserService.scala
@@ -1,110 +1,140 @@
 package services
 
-import play.api.Application
-import securesocial.core.{Identity, IdentityId, UserServicePlugin}
+import play.api.libs.concurrent.Execution.Implicits._
+import _root_.java.util.Date
+import securesocial.core._
+import play.api.{Logger,Application}
 import securesocial.core.providers.Token
-import scala.concurrent.Future
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.json.Writes._
+import securesocial.core.IdentityId
+import securesocial.core.providers.Token
+import play.modules.reactivemongo.MongoController
+import play.api.mvc.Controller
+import play.modules.reactivemongo.json.collection.JSONCollection
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import reactivemongo.core.commands.GetLastError
+import scala.util.parsing.json.JSONObject
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
 
-class MyUserService(application: Application) extends UserServicePlugin(application) {
+/**
+ * taken from http://www.shrikar.com/blog/2013/10/26/playframework-securesocial-and-mongodb/
+ *
+ * @param application
+ */
+class MyUserService(application: Application) extends UserServicePlugin(application) with Controller with MongoController{
+    def collection: JSONCollection = db.collection[JSONCollection]("users")
+    def tokens: JSONCollection = db.collection[JSONCollection]("tokens")
+    val outPutUser = (__ \ "id").json.prune
 
-  var users = Map[(String, String), DemoUser]()
-  private var tokens = Map[String, Token]()
+    def retIdentity(json : JsObject) : Identity = {
+      val userid = (json \ "userid").as[String]
 
-  /**
-   * Finds a user that maches the specified id
-   *
-   * @param id the user id
-   * @return an optional user
-   */
-  def find(id: IdentityId): Option[Identity] = {
-    val result = for (
-      user <- users.values;
-      basicProfile <- user.identities.find(pid => pid.identityId == id)
-    ) yield {
-      basicProfile
+      val provider = (json \ "provider").as[String]
+      val firstname = (json \ "firstname").as[String]
+      val lastname = (json \ "lastname").as[String]
+      val email = (json \ "email").as[String]
+      val avatar = (json \ "avatar").as[String]
+      val hash = (json \ "password" \ "hasher").as[String]
+      val password = ( json \ "password" \ "password").as[String]
+      println("password : "+ password)
+      val salt = (json \ "password" \ "salt").asOpt[String]
+      val authmethod = ( json \ "authmethod").as[String]
+
+      val identity : IdentityId = new IdentityId(userid,authmethod)
+      val authMethod : AuthenticationMethod = new AuthenticationMethod(authmethod)
+      val pwdInfo: PasswordInfo = new PasswordInfo(hash,password)
+      val user : SocialUser = new SocialUser(identity,firstname,lastname,firstname,Some(email),Some(avatar),authMethod,None,None,Some(pwdInfo))
+      user
     }
-    return None
-  }
 
-  /**
-   * Finds a user by email and provider id.
-   *
-   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
-   * implementation.
-   *
-   * @param email - the user email
-   * @param providerId - the provider id
-   * @return
-   */
-  def findByEmailAndProvider(email: String, providerId: String): Option[Identity] = {
-    val someEmail = Some(email)
-    val result = for (
-      user <- users.values;
-      basicProfile <- user.identities.find(su => su.identityId.providerId == providerId && su.email == someEmail)
-    ) yield {
-      basicProfile
+    def findByEmailAndProvider(email: String, providerId: String): Option[Identity] = {
+      val cursor  = collection.find(Json.obj("userid"->email,"provider"->providerId)).cursor[JsObject]
+      val futureuser = cursor.headOption.map{
+        case Some(user) => user
+        case None => false
+      }
+      val jobj = Await.result(futureuser, 5 seconds)
+      jobj match {
+        case x : Boolean => None
+        case _  => Some(retIdentity(jobj.asInstanceOf[JsObject]))
+
+      }
     }
 
-    return None
-  }
+    def save(user: Identity): Identity = {
 
-  /**
-   * Saves the user.  This method gets called when a user logs in.
-   * This is your chance to save the user information in your backing store.
-   * @param user
-   */
-  def save(user: Identity): Identity = {
-    // implement me
-    user
-  }
+      val email = user.email match {
+        case Some(email) => email
+        case _ => "N/A"
+      }
 
-  /**
-   * Saves a token.  This is needed for users that
-   * are creating an account in the system instead of using one in a 3rd party system.
-   *
-   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
-   * implementation
-   *
-   * @param token The token to save
-   */
-  def save(token: Token) = {
-    // implement me
-  }
+      val avatar = user.avatarUrl match{
+        case Some(url) => url
+        case _ => "N/A"
+      }
+
+      val savejson = Json.obj(
+        "userid" -> user.identityId.userId,
+        "provider" -> user.identityId.providerId,
+        "firstname" -> user.firstName,
+        "lastname" -> user.lastName,
+        "email" -> email,
+        "avatar" -> avatar,
+        "authmethod" -> user.authMethod.method,
+        "password" -> Json.obj("hasher" -> user.passwordInfo.get.hasher, "password" -> user.passwordInfo.get.password, "salt" -> user.passwordInfo.get.salt),
+        "created_at" -> Json.obj("$date" -> new Date()),
+        "updated_at" -> Json.obj("$date" -> new Date())
+      )
+      println(Json.toJson(savejson))
+      collection.insert(savejson)
+      user
+    }
+
+    def find(id: IdentityId): Option[Identity] = {
+      findByEmailAndProvider(id.userId,id.providerId)
+    }
+
+    def save(token: Token) {
+      val tokentosave = Json.obj(
+        "uuid" -> token.uuid,
+        "email" -> token.email,
+        "creation_time" -> Json.obj("$date" -> token.creationTime),
+        "expiration_time" -> Json.obj("$date" -> token.expirationTime),
+        "isSignUp" -> token.isSignUp
+      )
+      tokens.save(tokentosave)
+    }
 
 
-  /**
-   * Finds a token
-   *
-   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
-   * implementation
-   *
-   * @param token the token id
-   * @return
-   */
-  def findToken(token: String): Option[Token] = {
-    return None
-  }
 
-  /**
-   * Deletes a token
-   *
-   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
-   * implementation
-   *
-   * @param uuid the token id
-   */
-  def deleteToken(uuid: String) {
-    // implement me
-  }
+    def findToken(token: String): Option[Token] = {
 
-  /**
-   * Deletes all expired tokens
-   *
-   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
-   * implementation
-   *
-   */
-  def deleteExpiredTokens() {
-    // implement me
+      val cursor  = tokens.find(Json.obj("uuid"->token)).cursor[JsObject]
+      val futureuser = cursor.headOption.map{
+        case Some(user) => user
+        case None => false
+      }
+      val jobj = Await.result(futureuser, 5 seconds)
+      jobj match {
+        case x : Boolean => None
+        case obj:JsObject  =>{
+          println(obj)
+          val uuid = ( obj \ "uuid").as[String]
+          val email = (obj \ "email").as[String]
+          val created = (obj \ "creation_time" \ "$date").as[Long]
+          val expire = (obj \ "expiration_time" \ "$date").as[Long]
+          val signup = (obj \ "isSignUp").as[Boolean]
+          val df = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+          Some(new Token(uuid,email,new DateTime(created),new DateTime(expire),signup))
+        }
+      }
+    }
+
+    def deleteToken(uuid: String) {}
+
+    def deleteExpiredTokens() {}
   }
-}
