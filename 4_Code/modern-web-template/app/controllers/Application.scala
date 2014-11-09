ccf0affdd62a90d07fa5a064b06f082734111b1d
Joerg Amelunxen <jamelunx@mail.uni-paderborn.de>
2014-11-09 15:46:14 +0100

    [HiP-40]: controller names are changed

--- 4_Code/modern-web-template/app/controllers/Application.scala
+++ 4_Code/modern-web-template/app/controllers/Application.scala
@@ -11,13 +11,18 @@ class Application extends Controller  with SecureSocial{
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[Application])
 
+  def secureOption(x: Option[String], error:String) = x match {
+    case Some(s) => s
+    case None => error
+  }
+
   def index = SecuredAction { implicit  request =>
 
     println(" USER ---- ::: ")
     println(request.user)
     println(" USER ---- ::: ")
 
-    Ok(views.html.index())
+    Ok(views.html.index(secureOption(request.user.email,"ERROR: No email address specified")))
   }
 
   /*
