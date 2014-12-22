c6c7f0f95b5842fe6c771259a86402ab79c4af06
Jörg Amelunxen <jamelunx@mail.upb.de>
2014-12-22 15:05:34 +0100

    fix for last commit

--- 4_Code/modern-web-template/public/javascripts/AnnotationController.js
+++ 4_Code/modern-web-template/public/javascripts/AnnotationController.js
@@ -5,6 +5,11 @@
 controllersModule.controller('AnnotationCtrl', ['$scope','$http', '$routeParams', function($scope,$http,$routeParams) {
     var that = this;
 
+    /**
+     * This function enables the annotation functionality on the given DOM element
+     *
+     * @param id - The DOM element that should be used to start the annotation with
+     */
     this.startAnnotation = function(id){
         $(id).annotator('addPlugin', 'Store', {
             urls: {
